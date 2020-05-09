93f5ce8f5e5db2daaf65dc9a37be51c7a80bbd31
Dev Patel <dvptl68@gmail.com>
2020-05-09 10:33:55 -0400

    Implement multiplication mathematical script

---
+++ scripts/Mathematics/Multiplication.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+num=1
+i=0
+until [ "$num" = "" ]; do
+  read -p "Enter any number (or nothing to quit):"  num
+  if [ "$num" != "" ]; then
+    if ! [[ "$num" =~ ^[0-9]+$ ]]; then
+      echo "You must enter an integer!"
+    else
+      numbers[i]=$num
+      ((i++))
+    fi
+  fi
+done
+
+length=${#numbers[@]}
+((length--))
+total=1
+for j in $(seq 0 $length); do
+  ((total*=numbers[$j]))
+  echo -n ${numbers[$j]}
+  if [ "$j" != "$length" ]; then
+    echo -n " * "
+  fi
+done
+echo " = $total"
