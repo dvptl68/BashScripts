f0918e6d967511758be98520af744d23ceb32fe7
Dev Patel <dvptl68@gmail.com>
2020-05-09 10:39:38 -0400

    Implement division mathematical script

---
+++ scripts/Mathematics/Division.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+num=1
+i=0
+until [ "$num" = "" ]; do
+  read -p "Enter any number (or nothing to quit):"  num
+  if [ "$num" != "" ]; then
+    if ! [[ "$num" =~ ^[0-9]+$ ]]; then
+      echo "You must enter an integer!"
+    else
+      numbers[i]=$num
+      ((i++))
+    fi
+  fi
+done
+
+length=${#numbers[@]}
+((length--))
+for j in $(seq 0 $length); do
+  if [ "$j" != 0 ]; then
+    ((total/=numbers[$j]))
+  else
+    total=${numbers[$j]}
+  fi
+  echo -n ${numbers[$j]}
+  if [ "$j" != "$length" ]; then
+    echo -n " / "
+  fi
+done
+echo " = $total"
